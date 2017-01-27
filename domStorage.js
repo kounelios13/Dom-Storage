d63bfe3d5a776ecd224ca55d98774ab70a824c2e
kounelios13 <pkounelios@gmail.com>
2017-01-27 11:32:12 +0200

    created script

---
+++ domStorage.js
@@ -0,0 +1,21 @@
+function DomStorage() {
+	var cache = {};
+	function getFromCache(selector){
+		if(!cache[selector]){
+			cache[selector] = $(selector);
+		}
+		return cache[selector];
+	}
+	function updateInCache(selector){
+		if(!cache[selector]){
+			throw new TypeError("The selector you want to update does not exist in cache");
+		}
+		else{
+			cache[selector] = $(selector);
+		}
+	}
+	return {
+		get:getFromCache,
+		update:updateInCache
+	};
+}
